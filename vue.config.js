ad39b0554dcec4d004bc779fb5ba3d1270c9b349
Michael Umfahrer <michael@umfahrer.dev>
2022-11-10 15:05:28 +0100

    Fix build config for CI

--- vue.config.js
+++ vue.config.js
@@ -1,3 +1,3 @@
 module.exports = {
-    publicPath: process.env.NODE_ENV === 'production' ? '/vocabulary-trainer' : '/'
+    publicPath: process.env.NODE_ENV === 'production' ? (process.env.PUBLICPATH ? process.env.PUBLICPATH : '/') : '/'
 }
